4e1fcb3c2ae982750c8c9b02c8c973bbe4021538
mutmainahnida <nidaaa.m03@gmail.com>
2026-06-10 19:04:29 +0700

    feat:add omnichannel to navbar, switch default language to english inluding indonesian IP

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,12 +1,10 @@
 import { defineMiddleware } from 'astro:middleware';
 
 const LANG_MAP: Record<string, string> = {
-  ID: 'id',
-  SG: 'en', MY: 'en', AU: 'en', US: 'en', GB: 'en',
-  FR: 'fr', BE: 'fr', CH: 'fr', CA: 'fr',
+  FR: 'fr', BE: 'fr', CH: 'fr',
 };
 const DEFAULT_LANG = 'en';
-const SUPPORTED_LANGS = ['id', 'en', 'fr'];
+// const SUPPORTED_LANGS = ['id', 'en', 'fr'];
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
@@ -22,7 +20,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
     const country =
       context.request.headers.get('CF-IPCountry') ??
       context.request.headers.get('X-Vercel-IP-Country') ??
-      'ID';
+      'US';
 
     preferredLang = LANG_MAP[country.toUpperCase()] ?? DEFAULT_LANG;
 
@@ -35,7 +33,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
 (context.locals as any).lang = preferredLang;
 
-  // 2. JIKA USER MENGAKSES ROOT '/' -> LANGSUNG REDIRECT DI SERVER SIDE
   if (url.pathname === '/') {
     return context.redirect(`/${preferredLang}`, 302);
   }
